727f2d0ef4f333386d4e77bf1c8f77439dc5dbb1
Patrick Serr <serrp830@newschool.edu>
2018-02-04 16:40:31 -0500

    added random sizing

--- track-1/assets/js/app.js
+++ track-1/assets/js/app.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
     console.log( "ready!" );
     addElements();
     randomPosition();
+    randomSize();
 });
 
 
@@ -10,7 +11,7 @@ function addElement () {
   // create a new div element 
   var newDiv = document.createElement("div"); 
   // and give it some content 
-  var newContent = document.createTextNode("Hi there and greetings!"); 
+  var newContent = document.createTextNode("I'll be your mirror"); 
   // add the text node to the newly created div
   newDiv.appendChild(newContent);
   //creates and adds a class
@@ -22,32 +23,29 @@ function addElement () {
 }
 
 function addElements () {
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 10; i++) {
         addElement();
     }
 }
 
 
-// function randomPosition () {
-//     var left = Math.floor((Math.random() * 500) + 50);
-//     var top = Math.floor((Math.random() * 500) + 50);;
-//     $(".new").css("background-color", "yellow");
-//     $(".new").css("left", left);
-//     $(".new").css("top", top);
-//     console.log ("yellow");
-// }
-
 function randomPosition(){
     $( ".new" ).each(function() {
-        var left = Math.floor((Math.random() * 500) + 50);
-        var top = Math.floor((Math.random() * 500) + 50);;
+        var h = $(window).height();
+        var w = $(window).width();
+        var right = Math.floor((Math.random() * (4 * w / 5)) + 10);
+        var top = Math.floor((Math.random() * (2 * h / 3)) + 10);
         console.log( "hello" );
-        $(this).css("background-color", "yellow");
-        $(this).css("left", left);
+        $(this).css("right", right);
         $(this).css("top", top);
       });
 };
 
 function randomSize (){
-    
+    $( ".new" ).each(function() {
+    var width = Math.floor((Math.random() * 500) + 200);
+    var height = Math.floor((Math.random() * 500) + 200);
+    $(this).css("width", width);
+    $(this).css("height", height);
+    });
 }
