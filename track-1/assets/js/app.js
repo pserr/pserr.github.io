bea34e5434e8abdece7ef6882ad8eaee02e577c7
Patrick Serr <serrp830@newschool.edu>
2018-02-04 17:44:06 -0500

    user can escape divs

--- track-1/assets/js/app.js
+++ track-1/assets/js/app.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
     addElements();
     randomPosition();
     randomSize();
+    divHide();
 });
 
 
@@ -11,7 +12,7 @@ function addElement () {
   // create a new div element 
   var newDiv = document.createElement("div"); 
   //adds html to newDiv
-  $(newDiv).html("<img src='assets/img/your_mirror.svg'/>");
+  $(newDiv).html("<div class='esc'>X</div><img src='assets/img/your_mirror_3.svg'/>");
   //creates and adds a class
   $(newDiv).addClass("new");
   // add the newly created element and its content into the DOM 
@@ -21,7 +22,7 @@ function addElement () {
 }
 
 function addElements () {
-    for (i = 0; i < 15; i++) {
+    for (i = 0; i < 20; i++) {
         addElement();
     }
 }
@@ -32,18 +33,25 @@ function randomPosition(){
         var h = $(window).height();
         var w = $(window).width();
         var right = Math.floor((Math.random() * (4 * w / 5)) + 10);
-        var top = Math.floor((Math.random() * (2 * h / 3)) + 10);
-        console.log( "hello" );
+        var top = Math.floor((Math.random() * ( h / 3)) + 10);
+        // console.log( "hello" );
         $(this).css("right", right);
         $(this).css("top", top);
       });
 };
 
-function randomSize (){
+function randomSize(){
     $( ".new" ).each(function() {
     var width = Math.floor((Math.random() * 500) + 200);
     var height = Math.floor((Math.random() * 500) + 200);
     $(this).css("width", width);
     $(this).css("height", height);
+    });
+}
+
+function divHide(){
+    $(".esc").click(function(){
+        $(this).parent().css("display", "none");
+        // console.log("howdy");
     });
 }
