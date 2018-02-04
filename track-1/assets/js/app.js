7c470fd4743f9c9581802470611e229a14e7485e
Patrick Serr <serrp830@newschool.edu>
2018-02-04 15:54:31 -0500

    got random position working

--- track-1/assets/js/app.js
+++ track-1/assets/js/app.js
@@ -1,7 +1,7 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
-    addElement();
+    addElements();
     randomPosition();
 });
 
@@ -22,15 +22,32 @@ function addElement () {
 }
 
 function addElements () {
-    
+    for (i = 0; i < 5; i++) {
+        addElement();
+    }
 }
 
 
-function randomPosition () {
-    var left = Math.floor((Math.random() * 500) + 50);
-    var top = Math.floor((Math.random() * 500) + 50);;
-    $(".new").css("background-color", "yellow");
-    $(".new").css("left", left);
-    $(".new").css("top", top);
-    console.log ("yellow");
+// function randomPosition () {
+//     var left = Math.floor((Math.random() * 500) + 50);
+//     var top = Math.floor((Math.random() * 500) + 50);;
+//     $(".new").css("background-color", "yellow");
+//     $(".new").css("left", left);
+//     $(".new").css("top", top);
+//     console.log ("yellow");
+// }
+
+function randomPosition(){
+    $( ".new" ).each(function() {
+        var left = Math.floor((Math.random() * 500) + 50);
+        var top = Math.floor((Math.random() * 500) + 50);;
+        console.log( "hello" );
+        $(this).css("background-color", "yellow");
+        $(this).css("left", left);
+        $(this).css("top", top);
+      });
+};
+
+function randomSize (){
+    
 }
