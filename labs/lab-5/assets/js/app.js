755050de59c5ec53b34a313e6fa959f817387e2d
Patrick Serr <serrp830@newschool.edu>
2018-02-20 10:29:39 -0500

    added toggle to cart

--- labs/lab-5/assets/js/app.js
+++ labs/lab-5/assets/js/app.js
@@ -1,15 +1,33 @@
-$('.item').click(function){
-  if ($(this).hasClass('in-cart')){
-  alert('ALREADY IN CART');
-  var current_count = $('.item-count').html(),
-      current_value = Math.abs(current_count),
-      new_count     = current_value + 1;
+// $('.item').click(function) {
+//   if ($(this).hasClass('in-cart')) {
+//   alert('ALREADY IN CART')};
+
+//   var current_count = $('.item-count').html();
+//       current_value = Math.abs(current_count);
+//       new_count     = current_value + 1;
    
-    $('.item').html(new_count);
+//     $('.item').html(new_count);
 
-   $(this).addClass('in-cart');
+//     $(this).addClass('in-cart');
 
-}
+//   }
 
+//   };
+
+  $('.item').click(function() {
+    if ($(this).hasClass('in-cart')) {
+    console.log('ALREADY IN CART');
+    }
+    // var current_count = $('.item-count').html();
+    //     current_value = Math.abs(current_count);
+    //     new_count     = current_value + 1;
+     
+    //   $('.item').html(new_count);
   
-});
+    //   $(this).addClass('in-cart');
+  
+    });
+
+    $('.toggle').click(function() {
+      $('.cart').toggleClass('hide');
+    });
