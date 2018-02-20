2e800e17382c695bb2f69aeba1ce3522a2a6c2b7
Patrick Serr <serrp830@newschool.edu>
2018-02-20 09:31:38 -0500

    added Lab 5

---
+++ labs/lab-5/assets/js/app.js
@@ -0,0 +1,15 @@
+$('.item').click(function){
+  if ($(this).hasClass('in-cart')){
+  alert('ALREADY IN CART');
+  var current_count = $('.item-count').html(),
+      current_value = Math.abs(current_count),
+      new_count     = current_value + 1;
+   
+    $('.item').html(new_count);
+
+   $(this).addClass('in-cart');
+
+}
+
+  
+});
