0473ba01576683c10b023e0b23ef0b7761ba678e
Patrick Serr <serrp830@newschool.edu>
2018-02-27 08:59:59 -0500

    created lab 6

---
+++ labs/lab-6/assets/js/app.js
@@ -0,0 +1,45 @@
+// $('.item').click(function) {
+//   if ($(this).hasClass('in-cart')) {
+//   alert('ALREADY IN CART')};
+
+//   var current_count = $('.item-count').html();
+//       current_value = Math.abs(current_count);
+//       new_count     = current_value + 1;
+   
+//     $('.item').html(new_count);
+
+//     $(this).addClass('in-cart');
+
+//   }
+
+//   };
+
+  $('.item').click(function() {
+    if ($(this).hasClass('in-cart')) {
+    console.log('ALREADY IN CART');
+    }
+    // var current_count = $('.item-count').html();
+    //     current_value = Math.abs(current_count);
+    //     new_count     = current_value + 1;
+     
+    //   $('.item').html(new_count);
+  
+    //   $(this).addClass('in-cart');
+
+    //   add_to_cart(this);
+  
+    });
+
+    $('.toggle').click(function() {
+      $('.cart').toggleClass('hide');
+    });
+
+    function add_to_cart(line_item) {
+      //get img and price of item clicked
+      //inject html with img and price
+      // var price = $(this).
+      debugger;
+
+      var price = $(line_item).data('price'),
+          img = $(line_item).data('img');
+    };
