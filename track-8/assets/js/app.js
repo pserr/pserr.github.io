0718a8939c00512050253a21b9d84dd4b837cfab
Patrick Serr <serrp830@newschool.edu>
2018-03-04 18:45:15 -0500

    Added tracks 7 and 8

---
+++ track-8/assets/js/app.js
@@ -0,0 +1,28 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+});
+
+
+
+// function changeWord(divID) {
+//   nIntervId = setInterval(changeWordOnce, 500, divID);
+// }
+
+// function changeWordOnce(divID) {
+//   console.log(divID);
+//   var str = divID;
+//   var arr = str.slice(1);
+//   console.log(arr);
+//   var arr = $(arr);
+//   console.log(arr);
+//   var i = valueFromDeck();
+//   $(divID).html(quick[i]);
+
+//   window[arr]
+// }
+
+// function stopChange() {
+// clearInterval(nIntervId);
+// }
