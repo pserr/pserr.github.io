c4a818f4593c9d326aeda0e58340adb8eb520c64
Patrick Serr <serrp830@newschool.edu>
2018-03-04 21:35:36 -0500

    added link to track 8 on track 7

--- track-7/assets/js/app.js
+++ track-7/assets/js/app.js
@@ -1,28 +1,11 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-  console.log( "ready!" );
-
-});
 
 
+$(document).on("click", changePage);
 
-// function changeWord(divID) {
-//   nIntervId = setInterval(changeWordOnce, 500, divID);
-// }
+function changePage() {
+  location.href = "https://pserr.github.io/track-8/index.html";
+  
+}
 
-// function changeWordOnce(divID) {
-//   console.log(divID);
-//   var str = divID;
-//   var arr = str.slice(1);
-//   console.log(arr);
-//   var arr = $(arr);
-//   console.log(arr);
-//   var i = valueFromDeck();
-//   $(divID).html(quick[i]);
 
-//   window[arr]
-// }
-
-// function stopChange() {
-// clearInterval(nIntervId);
-// }
+// $(document).toggle("click", setPlaySpeed);
